92936085d55dd3e74df30fa5d5e954609ab81270
LucienLuc <lucienluc@umail.ucsb.edu>
2022-05-13 21:55:54 -0700

    Firebase testing

--- app/src/main/java/edu/ucsb/cs/cs184/group2/kiwi/MainActivity.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/group2/kiwi/MainActivity.kt
@@ -1,12 +1,19 @@
 package edu.ucsb.cs.cs184.group2.kiwi
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import edu.ucsb.cs.cs184.group2.kiwi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -31,5 +38,33 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val fab: FloatingActionButton = findViewById(R.id.floatingActionButton)
+        fab.setOnClickListener{
+            testFirebase()
+        }
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue()
+                Log.d("Test", "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("Test", "Failed to read value.", error.toException())
+            }
+        })
+    }
+    private fun testFirebase() {
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
     }
 }
