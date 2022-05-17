a63c0cfdf146e33251fb772f3c9e093a0ffde2fb
Alan Zhang <alan.zhang.052@gmail.com>
2022-05-17 15:06:13 -0700

    Implement Google SSO

---
+++ app/src/main/java/edu/ucsb/cs/cs184/group2/kiwi/ui/login/LoginViewModel.kt
@@ -0,0 +1,13 @@
+package edu.ucsb.cs.cs184.group2.kiwi.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is login Fragment"
+    }
+    val text: LiveData<String> = _text
+}
