53ad7525310f2c37e00195820c59000c4096777a
Reiley Batelaan <reileybatelaan@gmail.com>
2022-05-29 14:55:15 -0700

    merge with main

--- app/src/main/java/edu/ucsb/cs/cs184/group2/kiwi/ui/common/Event.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/group2/kiwi/ui/common/Event.kt
@@ -1,6 +1,7 @@
 package edu.ucsb.cs.cs184.group2.kiwi.ui.common
 
 class Event(
+    val firebase_id: String = "",
     val name: String = "",
     val date: String = "",
     val time: String = "",
